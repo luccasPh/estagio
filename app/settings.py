9f12c2c493dafd15ce25abb8d3519410edbd9aa2
Lucas <lucas4pinheiro@gmail.com>
2020-04-09 19:51:18 -0300

    deploy

--- app/settings.py
+++ app/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'sigea/static')
 
 STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'sigea/static'),
+     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 

609b3795826a193dc12954acb6e0249735dee0e4
Lucas <lucas4pinheiro@gmail.com>
2020-04-09 19:40:23 -0300

    deploy

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,6 +82,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -92,6 +94,9 @@ DATABASES = {
         'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
+"""
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation

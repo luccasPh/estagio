8725666ace6cdce7e0c7757da569dbd71bf87fd5
Lucas <lucas4pinheiro@gmail.com>
2020-04-10 16:16:07 -0300

    some adjustements

--- app/settings.py
+++ app/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-#import dj_database_url
-#import django_heroku
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -99,7 +99,7 @@ DATABASES = {
 """
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -155,7 +155,7 @@ LOGIN_URL = '/'
 LOGIN_REDIRECT_URL = '/'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 CLOUDINARY = {
     'cloud_name' : 'hndjdlnwz',
